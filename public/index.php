8143da207cd635a411db11dba361656eac5bed03
Gwinbleind <borislav31@ya.ru>
2020-07-10 23:39:58 +0300

    1. Баланс скобок

--- public/index.php
+++ public/index.php
@@ -1,45 +1,7 @@
-<?php
-
-use app\modules\MyStack;
-
-include '../modules/MyStack.php';
-include '../modules/MyStackElement.php';
-
-
-$close = [
-	'(' => ')',
-	'[' => ']',
-	'{' => '}',
-	'"' => '"',
-	"'" => "'"
-];
-$open = array_keys($close);
-$input = <<<INPUT
-"(")
-INPUT;
-
-$s = new MyStack();
-//$s = new SplStack();
-try {
-	while (!empty($input)) {
-		$char = $input[0];
-		if (in_array($char,$open)) {
-			$s->push($char);
-		} elseif (in_array($char, $close)) {
-			if ($s->isEmpty()) {
-				throw new RuntimeException('Miss open bracket', 0);
-			}
-			if ($close[$s->top()] == $char) {
-				$s->pop();
-			} else throw new RuntimeException('Brackets Mismatch',1);
-		}
-		$input = substr_replace($input,'',0,1);
-	}
-	if (!$s->isEmpty()) {
-		throw new RuntimeException('Miss close bracket',2);
-	}
-	echo 'true';
-}
-catch (RuntimeException $e) {
-	echo 'false: ' . $e->getMessage();
-}
+<h3>1. Баланс скобок</h3>
+<form method="post" action="brackets.php">
+	<input name="input" type="text">
+	<input type="submit">
+</form>
+<h3>2. Простые делители</h3>
+<h3>3. Проводник</h3>
