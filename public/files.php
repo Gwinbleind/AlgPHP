a20f10780ca0e0f845ab369a9e88cde2b1d7b668
Gwinbleind <borislav31@ya.ru>
2020-07-18 15:25:56 +0300

    Попробовал написать бинарный поиск с первого раза без IDE. Естественно, допустил несколько ошибок и переделал

--- public/files.php
+++
@@ -1,8 +0,0 @@
-<?php
-$startDir = empty($_GET['dir']) ? '/' : $_GET['dir'];
-
-$f = new DirectoryIterator($startDir);
-
-foreach ($f as $item):?>
-<a href="/files.php?dir=<?=$startDir.$item.'/'?>"><?=$item?></a><br>
-<?endforeach;?>
--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
 <h3>1. Баланс скобок</h3>
-<form method="post" action="brackets.php">
-	<input name="input" type="text">
-	<input type="submit">
-</form>
 <h3>2. Простые делители</h3>
-<form method="post" action="divider.php">
-	<input name="number" type="number">
-	<input type="submit">
-</form>
 <h3>3. Проводник</h3>
-<a href="/files.php">Файлы</a>
